849fa3ebfa2eecdd134501f90cef9315fe3f3c47
Industry666 <57324049+Industry666@users.noreply.github.com>
2021-03-15 13:23:28 +0700

    Delete appandDB_config.py

--- tools/appandDB_config.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-
-MY_BOT_TOKEN = '1685598178:AAFusprO5bzANmnrmLLGA8l2-RhUf1a9jdY'
-
-DB_CONFIG = psycopg2.connect(dbname='d5klq6gnk8r670', user='gumlnaazgcbgtn',
-                             password='0054f05b26ebbb84fc7caa2b5c7d39bd5c4ad9bcd4da8294b11ab847dabd2f4c',
-                             host='ec2-54-237-143-127.compute-1.amazonaws.com')
